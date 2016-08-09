2c905588c1ac5ecd48c900d7d3c4135dec90fd57
frecaze <frecaze@gmail.com>
2016-08-09 18:55:15 -0500

    agregando archivos de código en el repositorio

---
+++ codigo/js/my.js
@@ -0,0 +1,18 @@
+function suma(n1,n2,n3,n4,n5){
+    var resultado;
+    resultado = n1 + n2 +n3 + n4 + n5 ;
+    return resultado;
+}
+
+function calcularImpuesto(impuesto,ingreso){
+    var resultado;
+    resultado = impuesto * ingreso;
+    return resultado;
+}
+
+function promedio(n1,n2,n3){
+    var resultado;
+    resultado = (n1+n2+n3)/3;
+    return resultado;
+}
+
